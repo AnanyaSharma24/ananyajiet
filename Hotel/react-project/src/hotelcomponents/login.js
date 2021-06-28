537a4d83cb116105f8be3f493282a21f595653f7
AnanyaSharma24 <ananya.19jics012@jietjodhpur.ac.in>
2021-06-28 17:24:00 +0530

    hotel

--- Hotel/react-project/src/hotelcomponents/login.js
+++ Hotel/react-project/src/hotelcomponents/login.js
@@ -1,60 +1,54 @@
-import React from 'react';
-import LoginForm from './LoginForm';
-import { connect } from 'react-redux';
-import * as actions from 'actions';
-import {Redirect} from 'react-router-dom';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { setUserSession } from './Utils/Common';
 
-export class Login extends React.Component {
+function Login(props) {
+  const [loading, setLoading] = useState(false);
+  const username = useFormInput('');
+  const password = useFormInput('');
+  const [error, setError] = useState(null);
 
-    constructor(){
-        super();
+  const handleLogin = () => {
+    setError(null);
+    setLoading(true);
+    axios.post('http://localhost:4000/users/signin', { username: username.value, password: password.value }).then(response => {
+      setLoading(false);
+      setUserSession(response.data.token, response.data.user);
+      props.history.push('/dashboard');
+    }).catch(error => {
+      setLoading(false);
+      if (error.response.status === 401) setError(error.response.data.message);
+      else setError("Something went wrong. Please try again later.");
+    });
+  }
 
-        this.loginUser = this.loginUser.bind(this);
-    }
-
-    loginUser(userData){
-        this.props.dispatch(actions.login(userData));
-    }
-    render() {
-
-        const {isAuth, errors} = this.props.auth;
-        const {successRegistered} = this.props.location.state || false;
-
-        if(isAuth){
-            return <Redirect to={{pathname: '/rentals'}}/>
-        }
-        return (
-            <section id="login">
-                <div className="bwm-form">
-                    <div className="row">
-                        <div className="col-md-5">
-                            <h1>Login</h1>
-                            {
-                                successRegistered && 
-                                <div className="alert alert-success">
-                                    <p>You have been successfully registered. Please login now.</p>
-                                </div>
-                            }
-                            <LoginForm submitCb={this.loginUser} errors={errors}/>
-                </div>
-                        <div className="col-md-6 ml-auto">
-                            <div className="image-container">
-                                <h2 className="catchphrase">Hundreds of awesome places in reach of few clicks.</h2>
-                                <img src={process.env.PUBLIC_URL + '/img/login-image.jpg'} alt="" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
-        );
-    }
+  return (
+    <div>
+      Login<br /><br />
+      <div>
+        Username<br />
+        <input type="text" {...username} autoComplete="new-password" />
+      </div>
+      <div style={{ marginTop: 10 }}>
+        Password<br />
+        <input type="password" {...password} autoComplete="new-password" />
+      </div>
+      {error && <><small style={{ color: 'red' }}>{error}</small><br /></>}<br />
+      <input type="button" value={loading ? 'Loading...' : 'Login'} onClick={handleLogin} disabled={loading} /><br />
+    </div>
+  );
 }
 
-function mapStatetoProps(state){
-    return {
-        auth: state.auth
-    }
+const useFormInput = initialValue => {
+  const [value, setValue] = useState(initialValue);
+
+  const handleChange = e => {
+    setValue(e.target.value);
+  }
+  return {
+    value,
+    onChange: handleChange
+  }
 }
 
-export default connect(mapStatetoProps)(Login)
+export default Login;
